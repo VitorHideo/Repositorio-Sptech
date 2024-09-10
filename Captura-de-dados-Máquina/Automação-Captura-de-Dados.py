8a2a90d085ace569ed4d55e71608b7689a9d1f1a
VitorHideo <vitor.frutuoso@sptech.school>
2024-09-10 14:19:09 -0300

    livraria-java

--- Captura-de-dados-Máquina/Automação-Captura-de-Dados.py
+++ Captura-de-dados-Máquina/Automação-Captura-de-Dados.py
@@ -1,23 +1,24 @@
+# Script para rodar capturar dados do sistema da máquina, caso precise inserir no banco tire os comentários
+
 import psutil
 import time
-import mysql.connector
+# import mysql.connector
 import platform
+from socket import gethostname
 
-db_connection = mysql.connector.connect(host='localhost', user='root', password='947921', database='desafio')
-cursor = db_connection.cursor()
-
+i = 0
+nomeMaquina = gethostname()
 sistemaOperacional = platform.system()
 
-nomeMaquina = input("Escolha a máquina que você quer monitorar (m1, m2, m3, m4): ")
-componente = input("Escolha o componente que você deseja monitorar (cpu, memoria, disco): ")
-metrica = input("Escolha a métrica (a) percentual; b) bytes): ")
-tipoMedida = input("Escolha o tipo de medida (a) média por máquina; b) média total): ")
+# db_connection = mysql.connector.connect(host='host', user='usuario', password='senha', database='testeAutomacao')
+# cursor = db_connection.cursor()
 
 
-def capturaDeDados():
-    if sistemaOperacional == "Windows":
+while True:
+
+    if(sistemaOperacional=="Windows"):
         usoDeDisco = psutil.disk_usage('C:\\')
-    elif sistemaOperacional == "Linux":
+    elif(sistemaOperacional=="Linux"):
         usoDeDisco = psutil.disk_usage('/')
 
     usoDeCPU = psutil.cpu_percent(interval=1)
@@ -27,43 +28,52 @@ def capturaDeDados():
     qtdNucleos = psutil.cpu_count()
     qtdNucleosVirtuais = psutil.cpu_count(logical=False)
 
-    return usoDeCPU, freqDeCPU.current, usoDeMemo, usoDeDisco, qtdNucleos, qtdNucleosVirtuais
 
 
-def inserindoDados(usoDeCPU, freqDeCPU, usoDeMemo, usoDeDisco, qtdNucleos, qtdNucleosVirtuais):
-    sql = """INSERT INTO registros (nomeMaquina, sistemaOperacional, qtdTotalMemoria, percentualMemoria, qtdTotalDisco, qtdUtilizadaDisco, percentualDisco, qtdNucleosCPU, qtdNucleosVirtuaisCPU, percentualCPU, frequenciaCPU) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"""
-    
-    values = (nomeMaquina, sistemaOperacional, usoDeMemo.total, usoDeMemo.percent, usoDeDisco.total, usoDeDisco.used, usoDeDisco.percent, qtdNucleos, qtdNucleosVirtuais, usoDeCPU, freqDeCPU)
-    
-    cursor.execute(sql, values)
-    db_connection.commit()
+
+    i = i + 1
+
+    # sql = "INSERT INTO registros (nomeMaquina, sistemaOperacional, qtdTotalMemoria, percentualMemoria, qtdTotalDisco, qtdUtilizadaDisco, percentualDisco, qtdNucleosCPU, qtdNucleosVirtuaisCPU, percentualCPU, frequenciaCPU) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    # values = (nomeMaquina, sistemaOperacional, usoDeMemo.total, usoDeMemo.percent, usoDeDisco.total, usoDeDisco.used, usoDeDisco.percent, qtdNucleos, qtdNucleosVirtuais, usoDeCPU, freqDeCPU.current)
+    # cursor.execute(sql, values)
+    # db_connection.commit()
+
+    print(
+    """
+    {:d}º CAPTURA
+    ----------------------------------
+    Quantidade total de núcleos de CPU: {:d}
+    Quantidade total de núcleos virtuais de CPU: {:d}
+    Percentual de uso de CPU: {:.2f}%
+    Frequência da CPU: {:.2f} MHz
+
+    Quantidade total de memória: {:d} GB
+    Percentual de uso da memória: {:.2f}%
+
+    Quantidade total de disco: {:d} GB
+    Quantidade utilizada de disco: {:d} GB
+    Percentual de uso do disco: {:.2f}%
+
+    Nome da máquina: {:s}
+    Sistema Operacional: {:s}
+
+    """.format(i, qtdNucleos, qtdNucleosVirtuais, usoDeCPU, freqDeCPU.current, 
+               usoDeMemo.total, usoDeMemo.percent, 
+               usoDeDisco.total, usoDeDisco.used, usoDeDisco.percent, 
+               nomeMaquina, sistemaOperacional,))
+    time.sleep(5)
+
+    controle = 0
+
+    for process in psutil.process_iter():
+        print("\n"+"PROCESSOS DO SISTEMA")
+        print(f"ID: {process.pid}, Nome: {process.name()}, Status: {process.status()}")
+        controle += 1
+        if controle == 5:
+            break
+
+    print("##############################################################################################")
 
 
-def mostrarDados(usoDeCPU, usoDeMemo, usoDeDisco):
-    if componente == "cpu":
-        if metrica == "a":
-            print(f"Percentual de uso da CPU: {usoDeCPU:.2f}%")
-        else:
-            print(f"Frequência da CPU: {freqDeCPU.current:.2f} MHz")
-    elif componente == "memoria":
-        if metrica == "a":
-            print(f"Percentual de uso da memória: {usoDeMemo.percent:.2f}%")
-        else:
-            print(f"Total de memória: {usoDeMemo.total / (1024 ** 3):.2f} GB")
-    elif componente == "disco":
-        if metrica == "a":
-            print(f"Percentual de uso do disco: {usoDeDisco.percent:.2f}%")
-        else:
-            print(f"Total de disco: {usoDeDisco.total / (1024 ** 3):.2f} GB")
 
 
-def main():
-    usoDeCPU, freqDeCPU, usoDeMemo, usoDeDisco, qtdNucleos, qtdNucleosVirtuais = capturaDeDados()
-    inserindoDados(usoDeCPU, freqDeCPU, usoDeMemo, usoDeDisco, qtdNucleos, qtdNucleosVirtuais)
-    
-    print("\nMostrando dados coletados em tempo real:\n")
-    mostrarDados(usoDeCPU, usoDeMemo, usoDeDisco)
-
-while True:
-    main()
-    time.sleep(3)

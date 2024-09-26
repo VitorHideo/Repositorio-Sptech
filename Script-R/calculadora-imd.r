f675059744872a42e930485c395896e4174e4ed6
VitorHideo <vitor.frutuoso@sptech.school>
2024-09-25 22:11:56 -0300

    script - r

--- Script-R/calculadora-imd.r
+++ Script-R/calculadora-imd.r
@@ -1,24 +1,40 @@
 # Script IMC - Automação
-chave = 1
-while(chave > 0) {
-  peso = readline(prompt = "Digite o seu peso: ") 
-  peso = as.integer(peso)
-  altura = as.numeric(readline(prompt = "Digite a sua altura: "))
-  altura = as.double(altura)
-  IMC = peso/(altura * altura)
-  print(round(IMC, 1))
+dados <- data.frame(peso = numeric(), altura = numeric(), IMC = numeric(), diagnostico = character(), stringsAsFactors = FALSE)
+
+chave <- 1
+while (chave > 0) {
+  
+  peso <- readline(prompt = "Digite o seu peso: ") 
+  peso <- as.integer(peso)
+  altura <- as.numeric(readline(prompt = "Digite a sua altura: "))
+  altura <- as.double(altura)
+  
+  IMC <- peso / (altura * altura)
+  IMC_rounded <- round(IMC, 1)
   
   if (IMC < 18.5) {
-    print("Diagnóstico: Abaixo do peso normal")
+    diagnostico <- "Abaixo do peso normal"
   } else if (IMC >= 18.5 & IMC < 25) {
-    print("Diagnóstico: Peso normal")
+    diagnostico <- "Peso normal"
   } else if (IMC >= 25 & IMC < 30) {
-    print("Diagnóstico: Sobrepeso")
+    diagnostico <- "Sobrepeso"
   } else if (IMC >= 30 & IMC < 40) {
-    print("Diagnóstico: Obeso")
+    diagnostico <- "Obeso"
   } else if (IMC >= 40) {
-    print("Diagnóstico: Obesidade Mórbida")
+    diagnostico <- "Obesidade Mórbida"
   }
   
-  datasetIMC <- rbind(data_imc, data.frame(Peso = peso, Altura = altura, IMC = round(IMC, 1)))
+  print(paste("IMC:", IMC_rounded))
+  print(paste("Diagnóstico:", diagnostico))
+  
+  dados <- rbind(dados, data.frame(peso = peso, altura = altura, IMC = IMC_rounded, diagnostico = diagnostico, stringsAsFactors = FALSE))
+  
+  resposta <- readline(prompt = "Deseja continuar? (s/n): ")
+  
+  if (tolower(resposta) == "n") {
+    chave <- 0  
+  }
 }
+
+print("Dados armazenados:")
+print(dados)
